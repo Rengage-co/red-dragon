f25a3081cebf67e70dc91866121744ab2355dbd7
zhuhua <zhuhua@rengage.co>
2024-09-26 17:23:08 +0800

    update logger config

--- src/main/java/ai/rengage/logging/LogService.java
+++ src/main/java/ai/rengage/logging/LogService.java
@@ -1,27 +1,35 @@
 package ai.rengage.logging;
 
+import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogService {
     private static final Logger logger = LoggerFactory.getLogger(LogService.class);
+    private final String contextName;
+
+    public LogService() {
+        // 获取 LoggerContext 并读取 contextName
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        this.contextName = context.getName();
+    }
 
     public void info(String message) {
-        logger.info(getCallerInfo() + " - " + message);
+        logger.info("{} - {} - {}", contextName, getCallerInfo(), message);
     }
 
     public void debug(String message) {
-        logger.debug(getCallerInfo() + " - " + message);
+        logger.debug("{} - {} - {}", contextName, getCallerInfo(), message);
     }
 
     public void error(String message) {
-        logger.error(getCallerInfo() + " - " + message);
+        logger.error("{} - {} - {}", contextName, getCallerInfo(), message);
     }
 
     private String getCallerInfo() {
-        // stackTrace[2] 是调用 info/debug/error 方法的调用者
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        StackTraceElement caller = stackTrace[3];
+        StackTraceElement caller = stackTrace[3]; // 根据堆栈深度调整
         return caller.getClassName() + "." + caller.getMethodName() + "()";
     }
 }
+
