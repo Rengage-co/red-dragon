54bae0d5725ece91c696eb6d7dfc66e91d959b97
zhuhua <zhuhua@rengage.co>
2024-09-26 18:03:52 +0800

    update logger config

--- src/main/java/ai/rengage/logging/LogService.java
+++ src/main/java/ai/rengage/logging/LogService.java
@@ -1,29 +1,41 @@
 package ai.rengage.logging;
 
-import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class LogService {
     private static final Logger logger = LoggerFactory.getLogger(LogService.class);
+    private String propertyValue;
     private final String contextName;
 
     public LogService() {
         // 获取 LoggerContext 并读取 contextName
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         this.contextName = context.getName();
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+            if (input != null) {
+                properties.load(input);
+                propertyValue = properties.getProperty("spring.application.name", "rengage-logger");
+            }
+            if (null == propertyValue || propertyValue.isBlank() || propertyValue.isEmpty()){
+                propertyValue = contextName;
+            }
+        } catch (IOException e) {
+            logger.error("Error loading properties", e);
+        }
     }
 
     public void info(String message) {
-        logger.info("{} - {} - {}", contextName, getCallerInfo(), message);
-    }
-
-    public void debug(String message) {
-        logger.debug("{} - {} - {}", contextName, getCallerInfo(), message);
-    }
-
-    public void error(String message) {
-        logger.error("{} - {} - {}", contextName, getCallerInfo(), message);
+        logger.info("{} - {} - {}", propertyValue, getCallerInfo(),message);
     }
 
     private String getCallerInfo() {
@@ -31,5 +43,8 @@ public class LogService {
         StackTraceElement caller = stackTrace[3]; // 根据堆栈深度调整
         return caller.getClassName() + "." + caller.getMethodName() + "()";
     }
+
+    // 其他日志方法...
 }
+
 
