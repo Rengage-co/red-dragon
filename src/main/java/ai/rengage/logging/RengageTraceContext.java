733480234d46d12c770fedb00ccfef985247d9bb
zhuhua <zhuhua@rengage.co>
2024-11-05 14:16:31 +0800

    add support thread context  dependency

--- src/main/java/ai/rengage/logging/RengageTraceContext.java
+++ src/main/java/ai/rengage/logging/RengageTraceContext.java
@@ -1,7 +1,10 @@
 package ai.rengage.logging;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+
 public class RengageTraceContext {
-    private static final ThreadLocal<String> TRACE_ID = new ThreadLocal<>();
+//    private static final ThreadLocal<String> TRACE_ID = new ThreadLocal<>();
+    private static final TransmittableThreadLocal<String> TRACE_ID = new TransmittableThreadLocal<>();
 
     public static void setTraceId(String traceId) {
         TRACE_ID.set(traceId);
